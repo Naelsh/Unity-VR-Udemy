187fb99b5bdb169db4000b2bbc9ab2f1f46f63e6
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-01-24 20:06:54 +0100

    Video 35

--- Shoot Hoop/Shoot Hoop/Assets/Scripts/CameraRotation.cs
+++ Shoot Hoop/Shoot Hoop/Assets/Scripts/CameraRotation.cs
@@ -11,10 +11,11 @@ public class CameraRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float rotationSpeed = 30f;
+        float rotationSpeed = 60f;
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         
-        transform.rotation *= Quaternion.Euler(-(mouseY * Time.deltaTime * rotationSpeed), mouseX * Time.deltaTime * rotationSpeed, 0);
+        transform.rotation *= Quaternion.Euler(-(mouseY * Time.deltaTime * rotationSpeed), 0, 0);
+        transform.parent.transform.rotation *= Quaternion.Euler(0, mouseX * Time.deltaTime * rotationSpeed, 0);
 	}
 }
