79e7951e16bf7940f1c701b3984825c433f8f5b8
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-01-24 19:20:02 +0100

    Video 30

---
+++ Shoot Hoop/Shoot Hoop/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject basketBallPrefab;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(basketBallPrefab);
+        }
+	}
+}
