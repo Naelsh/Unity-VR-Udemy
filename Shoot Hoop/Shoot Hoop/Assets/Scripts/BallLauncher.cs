372984c223f1c342fee8cf99a1e656636896816b
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-01-24 20:33:30 +0100

    Video 36

---
+++ Shoot Hoop/Shoot Hoop/Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour {
+
+    public float ballVelocity = 5f;
+    public GameObject basketBallPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            InstantiateBall(Vector3.forward);
+        }
+    }
+
+    public void InstantiateBall(Vector3 direction)
+    {
+        GameObject basketball = Instantiate(basketBallPrefab);
+        basketball.transform.position = transform.position;
+        Rigidbody basketBallRB = basketball.GetComponent<Rigidbody>();
+        basketBallRB.velocity = direction * ballVelocity;
+    }
+}
