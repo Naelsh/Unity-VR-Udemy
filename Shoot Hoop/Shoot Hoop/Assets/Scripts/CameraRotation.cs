5f3458c0bddb4d641b81d1b6d53e7d4f68809b2f
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-01-24 19:41:02 +0100

    Video 32

---
+++ Shoot Hoop/Shoot Hoop/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.rotation = Quaternion.Euler(0,90,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation *= Quaternion.Euler(0, 10f* Time.deltaTime, 0);
+	}
+}
